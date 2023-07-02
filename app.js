7f58e9607d5164c9f8d5f0629181d5bc24b5d6e7
Imran Hussain <imranhussain399@gmail.com>
2023-07-02 17:07:53 +0500

    Add player vs computer mode to play againt computer

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const boardContainer = document.querySelector(".board-container");
 const playGameButton = document.querySelector("#play-game");
 const displayPlayerTurn = document.querySelector(".player-turn");
 const statsContainer = document.querySelector(".stats-container");
+const playMode = document.querySelector(".play-mode");
+playMode.onclick = function (e) {
+     console.log(`Play mode button activated`, e.target);
+     // let mode = getPlayMode();
+     // console.log(mode);
+     gameController.togglePlayMode();
+
+     playMode.textContent = gameController.getPlayMode();
+};
 // Add animation to board on content load...
 document.addEventListener("DOMContentLoaded", () => {
      gameContainer.classList.add("animate-container");
@@ -29,13 +38,39 @@ const gameBoard = (function () {
                boardContainer.children[i].textContent = "";
           }
      };
+     const resetGame = (mode) => {
+          resetBoard();
+          boardCellsArray = [];
+          if (mode === "P VS COMP") {
+               player1.resetScore();
+               computer.resetScore();
+               gameController.resetMatchDraw();
+               document.querySelector(".p1.score").textContent = "0";
+               document.querySelector(".tie-score").textContent = "0";
+               document.querySelector(".p2.score").textContent = "0";
+               document.querySelector(".p2.name").textContent = "PLAYER2";
+          } else {
+               player1.resetScore();
+               player2.resetScore();
+               gameController.resetMatchDraw();
+               document.querySelector(".p1.score").textContent = "0";
+               document.querySelector(".tie-score").textContent = "0";
+               document.querySelector(".p2.score").textContent = "0";
+               document.querySelector(".p2.name").textContent = "COMPUTER";
+          }
+     };
      const addCellToArray = (index, mark) => {
-          console.log(index, mark);
-          console.log(boardCellsArray[index] === undefined);
+          // console.log(index, mark);
+          // console.log(boardCellsArray[index] === undefined);
           if (boardCellsArray[index] === undefined) {
                boardCellsArray[index] = mark;
-          }
-          console.log("array elements", boardCellsArray);
+               return 0;
+          } else return "error";
+          // console.log("array elements", boardCellsArray);
+     };
+     const checkEmptyCell = (index) => {
+          if (boardCellsArray[index] === undefined) return true;
+          else return false;
      };
      const checkResult = () => {
           let cells = [...boardCellsArray];
@@ -82,13 +117,12 @@ const gameBoard = (function () {
           if (!isGameStarted) {
                isGameStarted = true;
                statsContainer.style.opacity = "1";
-               displayPlayerTurn.style.display = "block";
                displayPlayerTurn.style.opacity = "1";
                displayPlayerTurn.textContent = `${gameController.initialPlayer().name} Turn`;
                boardContainer.addEventListener("click", (event) => {
                     let index;
                     let targetCell;
-                    if (!(event.target.classList[0] === "game-container")) {
+                    if (!(event.target === boardContainer)) {
                          index = Number(event.target.dataset["cell"]);
                          targetCell = event.target;
                          gameController.addContentToBoard(index, targetCell);
@@ -97,7 +131,7 @@ const gameBoard = (function () {
           }
      };
 
-     return { createBoard, resetBoard, playGame, addCellToArray, checkResult };
+     return { resetGame, createBoard, resetBoard, playGame, addCellToArray, checkResult, checkEmptyCell };
 })();
 
 gameBoard.createBoard();
@@ -115,17 +149,60 @@ const createPlayer = ({ name, markPath, alt, width }) => {
      const updateScore = function () {
           playerScore++;
      };
+     const resetScore = () => {
+          playerScore = 0;
+     };
      const getScore = () => {
           return playerScore;
      };
-     return { name, mark, updateScore, getScore };
+     return { name, mark, updateScore, getScore, resetScore };
 };
 const markCircle = "./icons/circle-outline.svg";
 const markCross = "./icons/close-thick.svg";
-const player1 = createPlayer({ name: "Player1", markPath: markCross, alt: "cross", width: "70px" });
-const player2 = createPlayer({ name: "Player2", markPath: markCircle, alt: "circle", width: "56px" });
+const computer = (function () {
+     const { name, mark, updateScore, getScore, resetScore } = createPlayer({ name: "COMPUTER", markPath: markCircle, alt: "circle", width: "56px" });
+     const autoPlayTurn = () => {
+          let randomTurn = Math.floor(Math.random() * 9);
+
+          // console.log(`cell array is ${gameBoard.boardCellsArray}`);
+          // console.log(`cell at random turn ${randomTurn} is ${gameBoard.boardCellsArray[randomTurn]}`);
+          while (gameBoard.checkEmptyCell(randomTurn) === false) {
+               randomTurn = Math.floor(Math.random() * 9);
+          }
+          console.log("random turn", randomTurn);
+          // gameBoard.boardCellsArray[randomTurn] = computer.mark();
+          // boardContainer.children[randomTurn].append(computer.mark());
+          gameController.addContentToBoard(randomTurn, boardContainer.children[randomTurn]);
+     };
+
+     return { name, mark, updateScore, getScore, resetScore, autoPlayTurn };
+})();
+// console.log(computer);
+let player1 = createPlayer({ name: "PLAYER1", markPath: markCross, alt: "cross", width: "70px" });
+let player2 = createPlayer({ name: "PLAYER2", markPath: markCircle, alt: "circle", width: "56px" });
+// let computer = createPlayer({ name: "COMPUTER", markPath: markCircle, alt: "circle", width: "56px" });
 
 const gameController = (() => {
+     let mode = "P VS P";
+     const togglePlayMode = () => {
+          // Reset whole game
+          gameBoard.resetGame(mode);
+          // gameBoard.playGame();
+          if (mode === "P VS COMP") {
+               // player1 = createPlayer({ name: "PLAYER1", markPath: markCross, alt: "cross", width: "70px" });
+               // player2 = createPlayer({ name: "PLAYER2", markPath: markCircle, alt: "circle", width: "56px" });
+               // computer = null;
+               mode = "P VS P";
+          } else {
+               // player1 = createPlayer({ name: "PLAYER1", markPath: markCross, alt: "cross", width: "70px" });
+               // computer = createPlayer({ name: "COMPUTER", markPath: markCircle, alt: "circle", width: "56px" });
+               // player2 = null;
+               mode = "P VS COMP";
+          }
+     };
+     const getPlayMode = function () {
+          return mode;
+     };
      const initialPlayer = () => {
           return player1;
      };
@@ -146,15 +223,25 @@ const gameController = (() => {
                     currentPlayer = player1;
                     return 0;
                }
-               currentPlayer = playerTurn(currentPlayer);
-               displayPlayerTurn.textContent = `${currentPlayer.name} Turn`;
+               if (gameController.getPlayMode() === "P VS P") {
+                    currentPlayer = playerTurn(currentPlayer);
+                    displayPlayerTurn.textContent = `${currentPlayer.name} Turn`;
+               } else if (gameController.getPlayMode() === "P VS COMP" && currentPlayer === player1) {
+                    currentPlayer = playerTurn(currentPlayer);
+                    computer.autoPlayTurn();
+               } else if (gameController.getPlayMode() === "P VS COMP" && currentPlayer === computer) {
+                    currentPlayer = playerTurn(currentPlayer);
+                    displayPlayerTurn.textContent = `${currentPlayer.name} Turn`;
+               }
           }
      };
      // let initialPlayer = player1;
      const playerTurn = (currentPlayer) => {
           // console.log("current player", currentPlayer);
-          if (currentPlayer === player1) {
+          if (currentPlayer === player1 && gameController.getPlayMode() === "P VS P") {
                return player2;
+          } else if (currentPlayer === player1 && gameController.getPlayMode() === "P VS COMP") {
+               return computer;
           } else {
                return player1;
           }
@@ -166,18 +253,24 @@ const gameController = (() => {
           if (result === "win") {
                // console.log(`${currentPlayer.name} won the match`);
                currentPlayer.updateScore();
-               if (currentPlayer.name === "Player1") {
+               if (currentPlayer.name === "PLAYER1") {
                     // console.log(`current player is ${currentPlayer}`)
                     document.querySelector(".p1.score").textContent = currentPlayer.getScore();
                     modalMessage.style.top = "0px";
                     // modalMessage.style.pointerEvents = "all";
-                    announceText.textContent = `Player1 Won!`;
-               } else if (currentPlayer.name === "Player2") {
+                    announceText.textContent = `PLAYER1 WON!`;
+               } else if (currentPlayer.name === "PLAYER2") {
                     // console.log(`current player is ${currentPlayer}`)
                     document.querySelector(".p2.score").textContent = currentPlayer.getScore();
                     modalMessage.style.top = "0px";
                     // modalMessage.style.pointerEvents = "all";
-                    announceText.textContent = `Player2 Won!`;
+                    announceText.textContent = `PLAYER2 WON!`;
+               } else if (currentPlayer.name === "COMPUTER") {
+                    // console.log(`current player is ${currentPlayer}`)
+                    document.querySelector(".p2.score").textContent = currentPlayer.getScore();
+                    modalMessage.style.top = "0px";
+                    // modalMessage.style.pointerEvents = "all";
+                    announceText.textContent = `COMPUTER WON!`;
                }
           } else if (result === "tie") {
                updateMatchDraw();
@@ -205,12 +298,14 @@ const gameController = (() => {
      const getMatchDraw = () => {
           return tie;
      };
-     return { addContentToBoard, gameOver, updateMatchDraw, getMatchDraw, initialPlayer, playerTurn };
+     const resetMatchDraw = () => {
+          tie = 0;
+     };
+     return { togglePlayMode, getPlayMode, addContentToBoard, gameOver, updateMatchDraw, getMatchDraw, initialPlayer, playerTurn, resetMatchDraw };
 })();
 
 // console.log(gameController.playerTurn(player2))
 playGameButton.addEventListener("click", (e) => {
      gameBoard.playGame();
-     // console.log(e.target)
      playGameButton.textContent = "Restart Game";
 });
